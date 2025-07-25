3a39339602b400e499248ec76ed36297cfb2b62d
Jagan Behera <jaganbehera850@gmail.com>
2025-07-25 22:10:36 +0530

    day 2 of learning docker and terraform

--- terraform/terraform-practice-question/main.tf
+++ terraform/terraform-practice-question/main.tf
@@ -1,6 +1,7 @@
-resource "null_resource" "hello" {
- provisioner "local-exec"{
- command="echo hello terraform"
- }
- }
-
+resource "aws_instance" "ec2_instance"{
+ami="ami-042b4708b1d05f512"
+instance_type=var.instance_type
+tags={
+name="1st_ec2"
+}
+}
---
+++ terraform/terraform-practice-question/output.tf
@@ -0,0 +1,3 @@
+output "my_op"{
+value=aws_instance.ec2_instance.public_ip
+}
